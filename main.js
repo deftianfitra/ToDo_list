9ec2754a76ac6694da3666fe1a1eef35e1917082
Deftian Fitra Pradana <140016243+deftianfitra@users.noreply.github.com>
2023-09-16 00:13:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("#add").click(function () {
+        let nilaiInput = $("#input").val()
+        if (nilaiInput === "") {
+            alert("Anda belum memasukkan task!")
+        } else if (nilaiInput != "") {
+            $("#list").prepend(`<li><span>${nilaiInput}</span><button><i class="fa-regular fa-trash-can fa-xl"></i></button></li>`);
+        }
+        $('#input').val('');
+    });
+
+    $('#list').on('click', 'li button', function () {
+        $(this).parent('li').remove();
+    })
+
+    $('#list').on('click', 'span', function () {
+        $(this).toggleClass('garis');
+    })
+
+});
